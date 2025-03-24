fccd87eeccb43847e72aa1f48113a0bddce9bf60
“diegohazel” <ddovando@gmail.com>
2025-03-24 03:09:46 -0400

    iniciando el ejercicio 2 buscando la logica

---
+++ ejercicio2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+string x="red pepper";
+string word;
+char temp = 0;
+
+int contador = 0;
+
+const int mayus1 = 65, mayus2 = 90, minus1 = 97, minus2 = 122, espacio = 32;
+
+
+cout << "Ingrese sus dos palabras: "<< endl;
+//getline(cin, palabras);
+
+
+temp = x[contador];
+contador++;
+if (x[contador])
+
+
+/*do
+{
+    contador ++;
+
+    int letra;
+    letra = x[contador];
+
+    if ((letra >= mayus1 &&  letra <= mayus2) || (letra >= minus1 && letra <= minus2 || (letra == espacio)) )
+    {
+        cantidadLetras ++;
+    }else 
+    {
+        cantidadLetras ++;
+        
+        break;
+    }
+
+    if ((letra == espacio))
+    {
+            primeraPalabra = cantidadLetras;
+    }
+
+}while (contador < 100);
+
+
+*/
+
+    return 0;
+}
