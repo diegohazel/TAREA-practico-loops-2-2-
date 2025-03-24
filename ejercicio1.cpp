39606430059f7a0064cb1ccd432e1ccdf9382bf2
“diegohazel” <ddovando@gmail.com>
2025-03-24 02:30:29 -0400

    Agregando funcion de ERROR el Ingresar un espacio al inicio && LIMPLIANDO un poco el codigo de los comentarios que estorbaban a la vista

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -17,26 +17,29 @@ const int mayus1 = 65, mayus2 = 90, minus1 = 97, minus2 = 122, espacio = 32;
 // Espacio: 32 
 
 
-cout << "Ingrese 2 palabras: \n" << endl;
-//cin >> x;    ESTO NO FUNCIONA
-getline (cin, x);  //nota: Acordarme cada vez que voy a ingresar mas de 2 palabras en un string se usa el getline.
+
+do{
+    cout << "Ingrese 2 palabras: \n" << endl;
+    getline (cin, x);  
+    if (x[0] == espacio){
+        cout << "No se puede ingresar un espacio al inicio, VOLVER A INTENTAR"<< endl;
+    }
+}while(x[0] == espacio);
 
    do
     {
-        //cout << x[contador] << endl;
         contador ++;
 
         int letra;
         letra = x[contador];
-       // cout << letra << endl;
 
         if ((letra >= mayus1 &&  letra <= mayus2) || (letra >= minus1 && letra <= minus2 || (letra == espacio)) )
         {
             cantidadLetras ++;
         }else 
         {
-            cantidadLetras ++; //agregando esta linea para arreglar el bug de el reultado final.
-
+            cantidadLetras ++;
+            
             break;
         }
 
@@ -47,11 +50,7 @@ getline (cin, x);  //nota: Acordarme cada vez que voy a ingresar mas de 2 palabr
 
     }while (contador < 100);
 
-    //cout << "La primera palabra es : " << primeraPalabra << endl;
-    //cout << "cantidad de letras es : " << cantidadLetras << endl;
-    
     cout << primeraPalabra << endl;
-
     cout << cantidadLetras << endl;
 
 
