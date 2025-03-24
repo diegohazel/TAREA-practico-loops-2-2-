ece2a3626cec4a5468229117fe75980a1564c98e
“diegohazel” <ddovando@gmail.com>
2025-03-24 19:24:41 -0400

    Subiendo ejercicio 4 FINAL

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main(){
 
     int numero;
+    bool diego;
 
     cout << "Bienvenido al programa para CALCULAR SU FACTORIAL" << endl;
     
@@ -15,21 +16,40 @@ int main(){
         cout << "Ingrese su numero:" << endl;
         cin >> numero;
 
+        diego = true;
         if (numero < 0)
         {
             cout << "  El numero es negativo. Intentelo de nuevo" << endl;
+            diego = false;
         }else if (numero > 14)
         {
             cout << "El numero es muy grande. Intentelo de nuevo" << endl;
+            diego = false;
         }else if (numero == 0)
         {
             cout << "El numero es 0, tu resultado dara 0. Intentelo de nuevo" << endl;
-
+                diego = false;
         }
-    }while(numero <= 0 || numero > 14);
+    }while(diego == false);
 
-    
+int resultado = 1;
+int factorial;
+int numero2 = numero;
+int control = 1; 
+int control2 = numero - control;
+//resultado tiene que multiplicar por -1 hasta que CONTROL 2 == 0
 
+
+while(numero != control)
+{
+numero2 = numero2 * (numero - control);
+//resultado += factorial;
+//numero2 --;
+control++;
+resultado = numero2;
+}
+
+cout<< "El factorial de :\n "<< numero << " Es : \n" << resultado << endl;
 
     return 0;
 }
