adbddfbce61d603db4b36df59530f4ccfd2d9e73
“diegohazel” <ddovando@gmail.com>
2025-03-24 16:50:27 -0400

    Puliendo ultimos detalles a la seccion 1 del codigo

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -7,10 +7,12 @@ int main(){
 
     int numero;
 
+    cout << "Bienvenido al programa para CALCULAR SU FACTORIAL" << endl;
     
     do
     {
         
+        cout << "Ingrese su numero:" << endl;
         cin >> numero;
 
         if (numero < 0)
@@ -19,8 +21,14 @@ int main(){
         }else if (numero > 14)
         {
             cout << "El numero es muy grande. Intentelo de nuevo" << endl;
+        }else if (numero == 0)
+        {
+            cout << "El numero es 0, tu resultado dara 0. Intentelo de nuevo" << endl;
+
         }
-    }while(numero < 0 || numero > 14);
+    }while(numero <= 0 || numero > 14);
+
+    
 
 
     return 0;
